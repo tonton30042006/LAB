c2b9b2b7deb364a7f671df58097a2fd21d8ad43f
Kendu378 <ldminh2433@clc.fitus.edu.vn>
2025-10-17 23:12:39 +0700

    add input

--- Exercise 1/Part 3/H4.py
+++ Exercise 1/Part 3/H4.py
@@ -2,7 +2,7 @@
 
 import os
 
-filename = "filenamehere.extension"
+filename = input("Enter your file (format: filename.extension): ") # If not in folder then do foldername\filename.extension (can be multiple folder, example Exercise 1\Part 3\H2.py)
 
 if os.path.exists(filename):
     with open(filename, "r") as file:
